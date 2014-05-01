73ff8f64be53f242320cd752e8eb87693c757ebb
Kit Cross <chris.cross@guardian.co.uk>
2014-05-01 13:38:26 +0100

    Client side framework

---
+++ client/app.js
@@ -0,0 +1,5 @@
+(function () {
+	define(['backbone'], function(backbone) {
+
+	})
+})();
--- client/boot.js
+++ client/boot.js
@@ -1,9 +1,30 @@
 define([], function() {
 	'use strict';
+	var amdConfiguration = {
+		context: 'worldcup',
+		baseUrl: 'http://localhost/world-cup-predictions/client/',
+		paths: {
+			backbone: 'lib/backbone',
+			underscore: 'lib/underscore',
+			jquery: 'lib/jquery'
+		}
+	};
+
 	return {
-		boot : function() {
-			console.log('interactive loaded');
-			// document
+		boot : function(el, context, config, mediator) {
+			var launch;
+			var head = document.getElementsByTagName('head')[0];
+
+			var link = document.createElement('link');
+			link.setAttribute('rel', 'stylesheet');
+			link.setAttribute('href', 'http://localhost/world-cup-predictions/client/css/styles.css');
+			head.appendChild(link);
+
+			launch = function () {
+				app.launch();
+			};
+
+			require(amdConfiguration, ['app']).then(launch);
 		}
 	};
 });
