b9d5e0877e715e324e4c9e0f184c923a2e90090e
Kit Cross <chris.cross@guardian.co.uk>
2014-05-02 13:08:07 +0100

    Render the match schedule

--- client/app.js
+++ client/app.js
@@ -2,24 +2,27 @@ define([
 		'backbone',
 		'jquery',
 		'views/statusView',
+		'views/scheduleView',
 		'collections/scheduleCollection'
-	], function(backbone, $, StatusView, ScheduleCollection) {
+	], function(backbone, $, StatusView, ScheduleView, ScheduleCollection) {
+
 	var app = {};
 	return {
 		initialise: function () {
 
 			$(this.el).addClass('wcp');
 
-
 			// Schedule collection
-			var scheduleCollection = new ScheduleCollection;
-			scheduleCollection.fetch();
-
-			console.log(scheduleCollection);
+			var scheduleCollection = new ScheduleCollection();
 
 			var statusView = new StatusView();
-			$(this.el).html(statusView.render().el);
+			$(this.el).append(statusView.render().el);
 
+			var that = this;
+			scheduleCollection.fetch({success: function () {
+				var scheduleView = new ScheduleView({collection: scheduleCollection});
+				$(that.el).append(scheduleView.render().el);
+			}});
 		}
 	};
 });
