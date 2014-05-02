4038d744b1b75eca6e7e906831b843a095c7aacb
Kit Cross <chris.cross@guardian.co.uk>
2014-05-02 14:14:41 +0100

    Load template externally

--- client/views/matchView.js
+++ client/views/matchView.js
@@ -1,12 +1,10 @@
-define(['backbone'], function (Backbone) {
+define(['backbone', 'text!../templates/matchViewTemplate.html'], function (Backbone, MatchViewTemplate) {
     return Backbone.View.extend({
         tagName: 'li',
-        template: _.template('<div><img src="../client/images/<%= alphaCode %>.png"><%= alphaTeam %> vs <%= betaTeam %><img src="../client/images/<%= betaCode %>.png"></div>'),
-
+        template: _.template(MatchViewTemplate),
         render: function() {
         	$(this.el).html(this.template(this.model.toJSON()));
         	return this;
         }
-
     });
 });
