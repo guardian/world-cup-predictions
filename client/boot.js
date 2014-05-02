04efe9877bc857939095310dcdcbf393c2f7cd0c
Kit Cross <chris.cross@guardian.co.uk>
2014-05-02 14:13:27 +0100

    Remove CSS manual patching code

--- client/boot.js
+++ client/boot.js
@@ -1,25 +1,22 @@
 define([], function() {
 	'use strict';
-	
+
 	var amdConfiguration = {
 		context: 'worldcup',
-		baseUrl: 'http://localhost/world-cup-predictions/client/',
+		baseUrl: 'http://chronos.theguardian.com/world-cup-predictions/client/',
 		paths: {
 			backbone: 'lib/backbone',
 			underscore: 'lib/underscore',
 			jquery: 'lib/jquery'
-		}
+		},
+		pluginPath: 'lib/'
 	};
 
 	return {
 		boot : function(el, context, config, mediator) {
-			var head = document.getElementsByTagName('head')[0];
-			var link = document.createElement('link');
-			link.setAttribute('rel', 'stylesheet');
-			link.setAttribute('href', 'http://localhost/world-cup-predictions/client/css/styles.css?234098');
-			head.appendChild(link);
-
-			require(amdConfiguration, ['app']).then(function(app){app.el = el; app.initialise();});
+			require(amdConfiguration, ['app']).then(function(app){
+				app.el = el; app.initialise();
+			});
 		}
 	};
 });
