ce7dce0d2d0684ed53240e10cc5ba5e56f11fdc5
Kit Cross <chris.cross@guardian.co.uk>
2014-05-07 17:01:16 +0100

    Status view

--- client/views/statusView.js
+++ client/views/statusView.js
@@ -1,9 +1,10 @@
 define(['backbone'], function (Backbone) {
     return Backbone.View.extend({
-        template: _.template('<header>Highlight my favourite team. Your prediction score<div class=warning>2 matches today without predictions</div></header>'),
+        template: _.template('<header>Highlight my favourite team. Your prediction score<div class=warning>2 matches today without predictions. Your username </div></header>'),
 
         initialize: function () {
         	_.bindAll(this, 'render');
+        	this.model.on('change', this.render, this);
         	this.render();
         },
 
