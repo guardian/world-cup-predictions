604e080130798b9d1a4c7ad4424db123a9ac48df
Kit Cross <chris.cross@guardian.co.uk>
2014-05-09 16:10:40 +0100

    Template loading

--- client/views/timeView.js
+++ client/views/timeView.js
@@ -1,9 +1,12 @@
-define(['backbone', 'text!../templates/timeViewTemplate.html'], function (Backbone, TimeViewTemplate) {
+define([
+	'backbone',
+	'text!../templates/timeViewTemplate.html'
+	], function (Backbone, TimeViewTemplate) {
     return Backbone.View.extend({
         tagName: 'div',
         template: _.template(TimeViewTemplate),
         render: function() {
-        	$(this.el).html(this.template());
+        	$(this.el).html(this.template({ nextDate: 'Someday soon'}));
         	return this;
         }
     });
