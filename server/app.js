0f334debd14b692a5f92297d7ec24408d0d4d00a
Kit Cross <chris.cross@guardian.co.uk>
2014-05-21 18:02:09 +0100

    Fetch schedule data from database

--- server/app.js
+++ server/app.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var monk = require('monk');
-
+var cors = require('cors');
 var db = monk('localhost:27017/wcp');
-
 var app = express();
+var whitelist = ['http://chronos.theguardian.com'];
+var corsOptions = {
+	origin: function (origin, callback) {
+		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+		callback(null, originIsWhitelisted);
+	}
+};
+
+app.use(cors(corsOptions));
+app.listen(3000);
+
+app.get('/',function(req, res){
+  db.driver.admin.listDatabases(function(e,dbs){
+      res.json(dbs);
+  });
+});
 
 app.use(function(req, res, next) {
 	req.db = db;
 	next();
-})
-
-app.get('/', function(req, res) {
-  res.send('world cup prediction api here');
 });
 
-app.listen(3000);
+app.get('/schedule/', function(req, res) {
+	var schedule = db.get('schedule');
+
+	schedule.find({}, function(e, docs) {
+		res.json(docs);
+	});
+
+	// res.send(JSON.Stringify(schedule));
+});
