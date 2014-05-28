e510a79403785b9c71c3558f5c6cacd0ac4bc01f
Kit Cross <chris.cross@guardian.co.uk>
2014-05-28 18:05:37 +0100

    Collections of predictions

---
+++ client/collections/predictionCollection.js
@@ -0,0 +1,10 @@
+define(['backbone', 'models/prediction'], function (Backbone, Prediction) {
+    return Backbone.Collection.extend({
+		url: 'http://localhost:3000/prediction/',
+		model: Prediction,
+
+		getChanged: function () {
+			console.log('changed models');
+		}
+    });
+});
