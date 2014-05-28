1ca7491b1b40816337c3aa25337542e4b6f4ede2
Kit Cross <chris.cross@guardian.co.uk>
2014-05-28 18:02:30 +0100

    Match alpha and beta scores

--- client/models/match.js
+++ client/models/match.js
@@ -1,8 +1,8 @@
 define(['backbone'], function (Backbone) {
     return Backbone.Model.extend({
         defaults: {
-        	alphaTeam: 'Denmark',
-        	betaTeam: 'England'
+			alphaTeam: 'Denmark',
+			betaTeam: 'England'
         },
 
         initialize: function() {
