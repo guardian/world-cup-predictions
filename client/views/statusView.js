827ed03c7bf3b7839c86b40beb8d4cae1a43490e
Kit Cross <chris.cross@guardian.co.uk>
2014-06-10 22:03:43 +0100

    Import social model

--- client/views/statusView.js
+++ client/views/statusView.js
@@ -1,15 +1,31 @@
 define([
 		'backbone',
         'models/status',
-		'text!../templates/statusViewTemplate.html'
-	], function (Backbone, StatusModel, StatusViewTemplate) {
+		'text!../templates/statusViewTemplate.html',
+        'models/social'
+	], function (Backbone, StatusModel, StatusViewTemplate, SocialModel) {
     return Backbone.View.extend({
         template: _.template(StatusViewTemplate),
+
+        events: {
+            'click .wcp-facebook': 'clickFacebook',
+            'click .wcp-twitter': 'clickTwitter'
+        },
 
         initialize: function (options) {
             this.user = options.user;
             this.model = new StatusModel({id: this.user.get('userId')});
             this.model.on('change', this.render, this);
+        },
+
+        clickFacebook: function(e) {
+            e.preventDefault();
+            SocialModel.shareFacebook();
+        },
+
+        clickTwitter:function(e) {
+            e.preventDefault();
+            SocialModel.shareTwitter();
         },
 
         render: function() {
