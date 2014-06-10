2cc265956fbaac1045c18859344b6cb54f46c753
Kit Cross <chris.cross@guardian.co.uk>
2014-06-10 11:20:56 +0100

    Resolve merge conflict

--- client/collections/scheduleCollection.js
+++ client/collections/scheduleCollection.js
@@ -1,10 +1,6 @@
 define(['backbone', 'models/match'], function (Backbone, Match) {
     return Backbone.Collection.extend({
 		url: 'http://54.195.89.236:3000/schedule/',
-		// url: 'http://localhost:3000/schedule/',
-		model: Match,
-
-		getChanged: function () {
-		}
+		model: Match
     });
 });
