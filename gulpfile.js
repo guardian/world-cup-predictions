25ce8d755d612178d0c8c24b26138554f983a745
Kit Cross <chris.cross@guardian.co.uk>
2014-06-10 21:58:14 +0100

    Build process for r.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var minify = require('gulp-minify-css');
 var less = require('gulp-less');
 var nodemon = require('gulp-nodemon');
+var stripDebug = require('gulp-strip-debug');
+var rjs = require('gulp-requirejs');
 
 gulp.task('styles', function() {
 	return gulp.src('./client/less/styles.less')
@@ -24,4 +26,22 @@ gulp.task('server', function() {
 gulp.task('client', function() {
 	gulp.watch('./client/less/styles.less', ['styles']);
 	gulp.watch('./client/', ['templates']);
+});
+
+gulp.task('build', function() {
+	rjs({
+		baseUrl: 'client/',
+		out: 'app.js',
+		name: 'app',
+		inlineText: true,
+		paths: {
+			'backbone': 'lib/backbone',
+			'underscore': 'lib/underscore',
+			'jquery': 'lib/jquery',
+			'modal': 'lib/jquery.modal',
+			'text': 'lib/text',
+			'_fetchText': 'lib/_fetchText'
+		}
+	})
+	.pipe(gulp.dest('./build/'));
 });
