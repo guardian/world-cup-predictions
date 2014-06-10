32ee19649874a4238e615809255301cdab8495d3
Kit Cross <chris.cross@guardian.co.uk>
2014-06-10 22:01:05 +0100

    Add short url

--- client/config.js
+++ client/config.js
@@ -3,6 +3,7 @@ define([], function() {
 	'use strict';
 
 	return {
-		'apiUrl': 'http://54.195.89.236:3000'
+		'apiUrl': 'http://54.195.89.236:3000',
+		'shortUrl': 'http://gu.com/p/3qvyb'
 	};
 });
