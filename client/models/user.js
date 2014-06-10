2a0d7f8388e19edc492d8f91cc55a9c38a75be1d
Kit Cross <chris.cross@guardian.co.uk>
2014-06-10 10:22:27 +0100

    Get user e-mail

--- client/models/user.js
+++ client/models/user.js
@@ -3,6 +3,7 @@ define([
         'common/modules/identity/api'
         ], function (Backbone, Identity) {
         return Backbone.Model.extend({
+            urlRoot: 'http://54.195.89.236:3000/user',
             defaults: {
                 userID: 0,
                 isUserLoggedIn: false,
@@ -13,10 +14,17 @@ define([
                 this.set('isUserLoggedIn', Identity.isUserLoggedIn());
 
                 if (Identity.isUserLoggedIn()) {
-                    this.set('username', Identity.getUserFromCookie().displayName);
                     this.set('userId', parseInt(Identity.getUserFromCookie().id, 10));
+                    this.set('username', Identity.getUserFromCookie().displayName);
                     this.set('rawResponse', Identity.getUserFromCookie().rawResponse);
+                    this.set('userEmail', Identity.getUserFromCookie().primaryEmailAddress);
                 }
+
+                this.syncUserDetails();
+            },
+
+            syncUserDetails: function() {
+                this.save();
             }
 
         });
