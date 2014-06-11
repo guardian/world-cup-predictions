d95c1c6d71c7e231afcee1f1f12fb1620873bcb4
Kit Cross <chris.cross@guardian.co.uk>
2014-06-11 16:24:36 +0100

    Remove querystring check

--- client/app.js
+++ client/app.js
@@ -21,50 +21,46 @@ define([
 				$(this.el).addClass('wcp');
 				var appLoaded = false;
 				var querystring = window.location.search;
-				if (querystring.indexOf('?ok') !== 0) {
-					var comingSoonView = new ComingSoonView();
-					$(this.el).append(comingSoonView.render().el);
-				} else {
-					var scheduleCollection = new ScheduleCollection();
-					var user = new UserModel();
 
-					var usersPredictions = new PredictionModel({
-						id: user.get('userId'),
-						rawResponse: user.get('rawResponse')
+				var scheduleCollection = new ScheduleCollection();
+				var user = new UserModel();
+
+				var usersPredictions = new PredictionModel({
+					id: user.get('userId'),
+					rawResponse: user.get('rawResponse')
+				});
+				usersPredictions.fetch();
+
+				var modalView = new ModalView();
+				$(this.el).append(modalView.render().el);
+				$('.wcp-modal').modal();
+
+				Backbone.on('scoreChange', function() {
+					if (user.get('isUserLoggedIn') === false) {
+						$('.wcp-modal').trigger('openModal');
+					}
+
+					if (appLoaded)
+						usersPredictions.save({});
+				});
+
+				var statusView = new StatusView({user: user});
+				$(this.el).append(statusView.render().el);
+
+				var timeView = new TimeView();
+				$(this.el).append(timeView.render().el);
+
+				var that = this;
+				scheduleCollection.fetch({success: function () {
+					var scheduleView = new ScheduleView({
+						collection: scheduleCollection,
+						model: usersPredictions
 					});
-					usersPredictions.fetch();
 
-					var modalView = new ModalView();
-					$(this.el).append(modalView.render().el);
-					$('.wcp-modal').modal();
+					$(that.el).append(scheduleView.render().el);
 
-					Backbone.on('scoreChange', function() {
-						if (user.get('isUserLoggedIn') === false) {
-							$('.wcp-modal').trigger('openModal');
-						}
-
-						if (appLoaded)
-							usersPredictions.save({});
-					});
-
-					var statusView = new StatusView({user: user});
-					$(this.el).append(statusView.render().el);
-
-					var timeView = new TimeView();
-					$(this.el).append(timeView.render().el);
-
-					var that = this;
-					scheduleCollection.fetch({success: function () {
-						var scheduleView = new ScheduleView({
-							collection: scheduleCollection,
-							model: usersPredictions
-						});
-
-						$(that.el).append(scheduleView.render().el);
-
-						appLoaded = true;
-					}});
-				}
+					appLoaded = true;
+				}});
 
 			}
 		};
