590d485cb276327d973c6f7eea1884b68a69ae3c
daanlouter <daan.louter@gmail.com>
2014-06-11 11:50:44 +0100

    coming soon styling

--- client/collections/scheduleCollection.js
+++ client/collections/scheduleCollection.js
@@ -1,6 +1,7 @@
 define(['backbone', 'models/match'], function (Backbone, Match) {
     return Backbone.Collection.extend({
 		url: 'http://54.195.89.236:3000/schedule/',
+		// url: 'http://localhost:3000/prediction/',
 		model: Match
     });
 });
